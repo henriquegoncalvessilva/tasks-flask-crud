f08c651e26b1fa34bf224abe40823c879a3c178a
Biro <luizhenriquegoncalvessilva@hotmail.com>
2026-06-06 13:38:14 -0300

    add additional test cases for task update and delete operations

--- app_test.py
+++ app_test.py
@@ -1,4 +1,3 @@
-import pytest
 import requests
 
 BASE_URL = "http://127.0.0.1:5000"
@@ -14,6 +13,7 @@ def test_create_task():
     response_json = response.json()
     assert "message" in response_json
     assert "id" in response_json
+    tasks.append(response_json["id"])
 
 
 def test_get_tasks():
@@ -31,3 +31,36 @@ def test_get_task_by_id():
         assert response.status_code == 200
         response_json = response.json()
         assert task_id == response_json["id"]
+
+
+def test_update_task():
+    if tasks:
+        task_id = tasks[0]
+        payload_mock = {
+            "title": "Novo nome da tarefa",
+            "description": "Descrição Nova",
+            "completed": True,
+        }
+        response = requests.put(f"{BASE_URL}/tasks/{task_id}", json=payload_mock)
+        assert response.status_code == 200
+        response_json = response.json()
+        assert "message" in response_json
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+        response_json = response.json()
+        assert response_json["title"] == payload_mock["title"]
+        assert response_json["description"] == payload_mock["description"]
+        assert response_json["completed"] == payload_mock["completed"]
+
+
+def test_delete_task():
+    if tasks:
+        task_id = tasks[0]
+        response = requests.delete(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 200
+
+        response = requests.get(f"{BASE_URL}/tasks/{task_id}")
+        assert response.status_code == 404
+        response_json = response.json()
+        assert "message" in response_json
